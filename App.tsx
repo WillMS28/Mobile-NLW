c1322ceadbc4f604545db931927ffdc65b418812
WillMS28 <silva.miranda.william@gmail.com>
2020-12-06 17:35:37 -0300

    map-marker

--- App.tsx
+++ App.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps'
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps'
+
+import mapMarker from './src/images/map-marker.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} 
-      initialRegion={{
-
-      }} />
+      <MapView 
+        provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        initialRegion={{
+          latitude: -3.47018,
+          longitude: -41.5520965,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      >
+        <Marker 
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.5,
+            y: 0.8,
+          }}
+          coordinate={{
+            latitude: -3.47018,
+            longitude: -41.5520965
+          }}
+        >
+          <Callout tooltip onPress={() => {alert('hello') }}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar da Meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
@@ -21,5 +47,19 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: "center",
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
   }
 });
